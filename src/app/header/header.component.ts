52bf9c3ac1bd602611d7aa8ad352d17e9d034663
Mark Struckmann <mark.struckmann@yahoo.com>
2022-08-01 19:18:11 -0500

    working

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  missionName: string = "Mars 2030";
+  
+    missionName: string = "Mars 2030";
     rocketName: string = "Plasma Max";
 
   constructor() { }
